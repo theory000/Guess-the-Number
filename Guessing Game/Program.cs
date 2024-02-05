4210d9380eb7c1e506c395e9a182be55f60fad79
Salman Abid <theory252@gmail.com>
2024-02-05 22:17:33 +0500

    A guessing game

---
+++ Guessing Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GuessingGame // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+        }
+    }
+}
